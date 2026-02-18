cd3ba2b24bc6c75c915a25298f6d37d54caffbc4
muvlo <muvloroblox@gmail.com>
2026-02-18 15:22:59 -0600

    FLAG COMPLETE

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -6,6 +6,18 @@ import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
-		
+	
+	StdDraw.setPenColor(153,217,234);
+	StdDraw.filledRectangle(0.5,0.5,1,1);
+	StdDraw.rectangle(0.5, 0.5, 0.3, 0.2);
+	StdDraw.setPenColor(0,0,0);
+	StdDraw.rectangle(0.5, 0.5, 0.3, 0.2);		
+	StdDraw.setPenColor(162,8,255);
+	StdDraw.filledRectangle(0.5, 0.5, 0.3, 0.2);
+	StdDraw.setPenColor(255,255,255);
+	StdDraw.filledRectangle(0.5, 0.5, 0.3, (0.2/3));
+	StdDraw.setPenColor(100,237,186);
+	StdDraw.filledCircle(0.5, 0.5, (0.2/3));
+
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -20,5 +20,48 @@ public class InterpretDrawingFile {
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
 		
+		String shapeType = in.next();
+		int redComponent = in.nextInt();
+		int greenComponent = in.nextInt();
+		int blueComponent = in.nextInt();
+		boolean isFilled = in.nextBoolean();
+		
+		double parameterOne = in.nextDouble(); // x1
+		double parameterTwo = in.nextDouble(); // y1
+		double parameterThree = in.nextDouble(); // x2
+		double parameterFour = in.nextDouble(); // y2
+
+		if (shapeType.equals("triangle")) {
+			double parameterFive = in.nextDouble(); // x3
+			double parameterSix = in.nextDouble(); // y3
+			double[] x = {parameterOne, parameterThree, parameterFive};
+			double[] y = {parameterTwo, parameterFour, parameterSix};
+
+			if (isFilled) {
+				StdDraw.filledPolygon(x, y);
+			}
+			else {
+				StdDraw.polygon(x, y);
+			}
+		}
+
+		StdDraw.setPenColor(redComponent, greenComponent, blueComponent);
+
+		if (isFilled) {
+			if (shapeType.equals("rectangle")) {
+				StdDraw.filledRectangle(parameterOne, parameterTwo, parameterThree, parameterFour);
+			}
+			else if (shapeType.equals("ellipse")) {
+				StdDraw.filledEllipse(parameterOne, parameterTwo, parameterThree, parameterFour);
+			}
+		}
+		else {
+			if (shapeType.equals("rectangle")) {
+				StdDraw.rectangle(parameterOne, parameterTwo, parameterThree, parameterFour);
+			}
+			else if (shapeType.equals("ellipse")) {
+				StdDraw.ellipse(parameterOne, parameterTwo, parameterThree, parameterFour);
+			}
+		}
 	}
 }
